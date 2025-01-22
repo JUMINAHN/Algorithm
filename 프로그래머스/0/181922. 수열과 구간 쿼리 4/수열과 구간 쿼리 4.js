21c12417c2a25069c53b94c2dcda60113b12386b
JUMIN AHN <140716804+JUMINAHN@users.noreply.github.com>
2025-01-22 20:51:04 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 28.29 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -1,18 +1,8 @@
 function solution(arr, queries)  {
-    //s,e,k
-    //s <= i <= e인 모든 i에 대해 i가 k의 배수이면..?? => 그냥 딱 i가 배수인지만 확인
-    //0은 무조건 포함 idx % k === 0이어야 함
-    // arr[i]에 1을 더한다. => 약수이면
-    for ([s,e,k] of queries) {
-        // console.log(s,e,k)
-        arr.forEach((num, i) => {
-            if((s <= i && i <= e) && (i % k == 0)) {
-                // console.log(i)
-                arr[i] += 1
-            }
-        })
-        //여기서 다시 올라갈건데 arr다시 확인
-        // console.log(arr)
-    }
-    return arr
-  }
+    return queries.reduce((acc, [s,e,k]) =>  {
+        for (let i=s ; i <=e ; i++) {
+            if( i % k === 0) acc[i] += 1//기존값에 누적
+        }
+        return acc
+    }, [...arr])
+}
