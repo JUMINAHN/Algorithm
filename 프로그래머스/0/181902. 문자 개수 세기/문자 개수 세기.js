f9a68d9777e6767acbe22cc82c9e66f28839a5c9
JUMIN AHN <140716804+JUMINAHN@users.noreply.github.com>
2025-01-28 13:16:03 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.33 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    //A부터 ... z까지 담은 배열을 return 
+    //노가다로 A부터 Z까지 하는방법밖에 모르겠는데.. => count정렬
+    const data = {A : 0, B : 0, C : 0, D : 0, E : 0, F : 0, G :0 , H:0, I:0,J : 0, K:0, L:0, M:0
+            ,N : 0, O : 0, P : 0, Q : 0, R: 0 , S : 0, T : 0, U : 0, V : 0, W :0, X :0, Y:0, Z:0,
+                 a : 0, b : 0, c :0, d: 0, e:0, f:0, g:0 , h:0, i:0, j:0, k:0, l:0, m:0, n:0, o:0
+            , p:0, q:0, r:0, s:0, t:0, u:0, v:0, w:0, x:0, y:0, z:0}     
+        //Object.keys(data) //key값들만
+        let newStr = [...my_string].forEach((item) => (data[item] += 1))
+        return Object.values(data)
+    
+}
