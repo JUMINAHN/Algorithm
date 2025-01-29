da7d51e9938941c5bc97f6b700e4a8a8ef666676
JUMIN AHN <140716804+JUMINAHN@users.noreply.github.com>
2025-01-29 12:01:12 +0900

    [level 0] Title: 배열 조각하기, Time: 2.07 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -1,11 +1,35 @@
 function solution(arr, query) {
-    //query의 홀짝여부를 판단하는것?
-    for (let i = 0; i < query.length; i++) {
-        if (i % 2 === 0) { //짝수이면
-            arr = arr.slice(0, query[i] + 1); //0부터 query[i까지]
-        } else { //홀수이면?
-            arr = arr.slice(query[i]); //query[i-1]은제거하니까 query[i]부터 뒤까지
+    //query를 순회하면서
+    //짝수 idx에서는 arr에서 query[i]번 인덱스를 제외하고, query[i] 뒷 부분을 잘라서 버림
+    //홀수 idx에서는 arr에서 query[i]번 인덱스를 제외하고, query[i] 앞 부분을잘라서 버림
+    //그니까 지금 배열의 수는 계속 줄어들게되고, query를 순회할 때 나오는 q는 => arr[i]의 i가 되는 것
+    //근데 q가 => idx고 => idx에 맞게 홀 짝을 구하면서 되는 것
+    
+    // for (q of query) {
+    //     if (q % 2 === 0) {
+    //         //특정 함수 제거..?
+    //         //짝
+    //         arr = 
+    //     } else {
+    //         //홀
+    //     }
+    // }
+   
+    //pop도 아니었는데 => 특정함수가 제거되지 않아서 아마 replace로 "" => str
+    //reduce는 누적인데 괜찮을까? 
+    const result = query.reduce((acc, idx) => {
+        //일단 초기값은 arr
+        console.log(acc) //첫번째에는 값이 나옴
+        if (idx % 2 === 0) {
+            //replace가 str타입?
+            //짝수
+            return acc[idx+1] = ''
+            // acc.replace(acc[idx+1], "")
+        } else {
+            //홀수
+            return acc[idx-1] =''
+            // acc.replace(acc[idx-1], "")
         }
-    }
-    return arr;
+    },arr)
+    console.log(result)
 }
