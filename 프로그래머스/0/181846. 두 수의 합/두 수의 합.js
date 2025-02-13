2c0d10f4a57e56e6379dc2ef8bdb9f6e99eac180
JUMIN AHN <140716804+JUMINAHN@users.noreply.github.com>
2025-02-13 21:52:45 +0900

    [level 0] Title: 두 수의 합, Time: 105.19 ms, Memory: 34.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.js
@@ -0,0 +1,8 @@
+function solution(a, b) {
+    const idxA = BigInt(a)
+    const idxB = BigInt(b)
+    // console.log(idxA, idxB) //특정 number는 Number이 안됨 === parseInt
+    // console.log(idxA+idxB)
+    return String(idxA+idxB)
+    
+}
